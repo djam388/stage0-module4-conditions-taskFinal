111744ca3d5055939356143058759cfbd14de434
djam388 <djam388@hotmail.com>
2022-08-02 11:06:07 +0500

    Attempt #3

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,13 +2,13 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public static void main(String[] args) {
-        printCompletelyDivided(5, 2);
+        printCompletelyDivided(25, 0);
     }
     public static void printCompletelyDivided(int dividend, int divider) {
         int result;
-        result = dividend / divider;
 
         if (divider != 0) {
+            result = dividend / divider;
             if (divider * result == dividend) {
                 System.out.println("can be divided completely");
             }
